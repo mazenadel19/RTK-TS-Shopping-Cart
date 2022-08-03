cfd95f605b510ee722a1b165c4bc1b6c88eaf3a8
Mazen <mazenadel19@gmail.com>
2022-08-04 01:19:12 +0200

    removing post request to handle 405  status when checkout

--- src/app/api.ts
+++ src/app/api.ts
@@ -9,29 +9,25 @@ export interface Product {
 }
 
 export async function getProducts(): Promise<Product[]> {
-	const results = await fetch('/products.json')
-	const products = results.json()
-	return products
+  const results = await fetch("/products.json");
+  const products = results.json();
+  return products;
 }
 
 export type CartItems = { [productID: string]: number };
 export type CheckoutResponse = { success: boolean; error?: string };
 
 export async function checkout(items: CartItems): Promise<CheckoutResponse> {
-	const modifier = Object.keys(items).length > 0 ? 'success' : 'error'
-	const url = `/checkout-${modifier}.json`
-	await sleep(500)
-	const response = await fetch(url, {
-		method: 'POST',
-		body: JSON.stringify(items),
-	})
-	const data = await response.json()
-	if (!data.success) {
-		throw new Error(data.error)
-	}
-	return data as CheckoutResponse
+  const modifier = Object.keys(items).length > 0 ? "success" : "error";
+  const url = `/checkout-${modifier}.json`;
+  await sleep(500);
+  const response = await fetch(url);
+  const data = await response.json();
+  if (!data.success) {
+    throw new Error(data.error);
+  }
+  return data as CheckoutResponse;
 }
 
 // utility function to simulate slowness in an API call
-const sleep = (time: number) =>
-	new Promise((res) => setTimeout(res, time))
+const sleep = (time: number) => new Promise((res) => setTimeout(res, time));
